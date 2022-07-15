dd56563564a06856c4fd8d449717a5e34622a514
Matt <38109405+mmcguire91@users.noreply.github.com>
2022-07-15 16:21:51 -0500

    mint an NFT!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
+import { ethers } from 'ethers';
+import WhiskeyNFT from './utils/WhiskeyNFT.json'
 
 // Constants
 const TWITTER_HANDLE = 'whiskey_network';
@@ -13,7 +15,7 @@ const App = () => {
   //state variable to store user's public wallet. useState is dependency
   const [currentAccount, setCurrentAccount] = useState("");
 
-  const checkIfWalletConencted = async () => {
+  const checkIfWalletConnected = async () => {
     //make sure we have access to window.ethereum
     const { ethereum } = window;
 
@@ -61,16 +63,55 @@ const App = () => {
     }
   }
 
+  const askContractToMint = async () => {
+    //Deployed contract address
+    //every time contract is deployed, this AND abi needs to be updated
+    const CONTRACT_ADDRESS = "0xa1631f0460826E5DBE8748D971d3C480681758Aa"; 
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        //ethers is a library that helps our frontend talk to our contract
+        //provider talks to eth nodes
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        //creates the connection to our contract 
+        //contract's address, abi file, and signer are the 3 things are always needed to communicate with contracts on the blockchain.
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, WhiskeyNFT.abi, signer);
+        /*
+        * abi 
+        * abi is retrieved after contract is deployed.
+        * retrieved from smart contract repo in artifacts/[CONTRACT_NAME].sol/[CONTRACT_NAME].json, copy contents
+        * create new path utils/[CONTRACT_NAME].json
+        * import here
+        */
+
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeWhiskeyNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+      } else {
+        console.log("Ethereum object does not exist. You don't have anything to connect to ethereum installed.");
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
 
   //runs function while page loads
   useEffect(() => {
-    checkIfWalletConencted();
+    checkIfWalletConnected();
   }, [])
 
   return (
@@ -84,7 +125,7 @@ const App = () => {
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
+            <button onClick={askContractToMint} className="cta-button connect-wallet-button">
               Mint NFT
             </button>
           )}
